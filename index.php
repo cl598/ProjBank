96ff2fd09457e3fdc2656ff7be7bb938d15edfe8
cl598 <55933747+cl598@users.noreply.github.com>
2020-12-03 12:25:54 -0500

    Update and rename Project/index.php to index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="static/css/styles.css">
-<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php require_once(__DIR__ . "Project/partials/nav.php"); ?>
 This is a placeholder index file for the project.
